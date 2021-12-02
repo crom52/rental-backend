154fcf0a50f7044f318a8d83af13df1b3ec320ce
unknown <phuc52crom@gmail.com>
2021-12-02 23:43:42 +0700

    fix

--- src/main/java/com/crom/mybatis/config/DataSourceConfig.java
+++ src/main/java/com/crom/mybatis/config/DataSourceConfig.java
@@ -11,11 +11,11 @@ public class DataSourceConfig {
 	
     @Bean
     public DataSource getDataSource() {
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
-        dataSourceBuilder.url("jdbc:mysql://us-cdbr-east-04.cleardb.com/heroku_e556888335116c5?reconnect=true&sslmode=require");
-        dataSourceBuilder.username("b6ecaef51dbee5");
-        dataSourceBuilder.password("1204900e");
+        dataSourceBuilder.url("jdbc:mysql://sql6.freemysqlhosting.net/sql6455912?reconnect=true&ssl=true&sslmode=require");
+        dataSourceBuilder.username("sql6455912");
+        dataSourceBuilder.password("7sagEAXmMz");
         return dataSourceBuilder.build();
     }
 }
